723f1891119982d6559948bd08e019cc7be31b33
DemiAina <100835583+DigitalDemi@users.noreply.github.com>
2024-11-12 04:08:22 +0000

    nuke and start again

--- src/core/Server/client_state.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ClientState(Enum):
-    CONNECTED = "connected"
-    REGISTERED = "registered"
-    ACTIVE = "active"
-    INACTIVE = "inactive"
-    DISCONNECTED = "disconnected"
--- src/core/Server/client_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ClientType(Enum):
-    BUYER = "buyer"
-    SELLER = "seller"
